046449c4da44a268fb8fa794a879008cd79b4e36
liam-mcallister <liammca1987@gmail.com>
2020-02-09 20:49:52 +0000

    Created why us section in index.html / nav disappears on scroll

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    // When the page is scrolled 50px or more from the top of the page, fade the 'back to top' button in
+    // When the page is scrolled to within 50px to the top of the page, fade the 'back to top' button out
     $(window).scroll(function () {
         if ($(window).scrollTop() > 50) {
             $('#back-to-top').fadeIn();
@@ -8,11 +10,27 @@ $(document).ready(function () {
         }
     });
 
+    // When the 'back to top' button is clicked, scroll to the top of the page
     $('#back-to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 700);
         return false;
     });
+
+    // When the user scrolls down, slide the navbar out of view
+    // When the user scrolls up, slide the navbar into view
+    var lastPos = 0;
+    $(window).scroll(function (event) {
+        var currentPos = $(this).scrollTop();
+        if (currentPos > lastPos) {
+            $('#navbar').slideUp();
+        }
+        else {
+            $('#navbar').slideDown();
+        }
+        lastPos = currentPos;
+    });
+
 
 });
